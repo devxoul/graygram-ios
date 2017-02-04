10fa2b19ce11dd4d9d9d674e1d7e7f90c4d54002
Suyeol Jeon <devxoul@gmail.com>
2017-02-05 03:01:54 +0900

    Initial commit :)

---
+++ Graygram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Graygram
+//
+//  Created by Suyeol Jeon on 05/02/2017.
+//  Copyright © 2017 Suyeol Jeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# Graygram for iOS
