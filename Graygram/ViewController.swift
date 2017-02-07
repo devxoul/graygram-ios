e3d0cfd210d9e10a184d046e5a585654b6e50fc7
Suyeol Jeon <devxoul@gmail.com>
2017-02-08 04:16:00 +0900

    Alamofire를 사용해서 GET 요청을 보내봄

--- Graygram/ViewController.swift
+++ Graygram/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+
+    Alamofire.request("https://api.graygram.com/feed").responseJSON { response in
+      print(response)
+    }
   }
 
   override func didReceiveMemoryWarning() {
