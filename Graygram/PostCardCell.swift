f367b35b5567fe150cd1d2b12110cf8c5fe4b2ae
Suyeol Jeon <devxoul@gmail.com>
2017-02-10 18:27:05 +0900

    PostCardCell 클래스 추가

---
+++ Graygram/PostCardCell.swift
@@ -0,0 +1,21 @@
+//
+//  PostCardCell.swift
+//  Graygram
+//
+//  Created by Suyeol Jeon on 08/02/2017.
+//  Copyright © 2017 Suyeol Jeon. All rights reserved.
+//
+
+import UIKit
+
+final class PostCardCell: UICollectionViewCell {
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+}
