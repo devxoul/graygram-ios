0491146915948170a06316edf631877de6c90cf2
Suyeol Jeon <devxoul@gmail.com>
2017-02-10 19:12:24 +0900

    PostCardCell에 photoView 추가

--- Graygram/PostCardCell.swift
+++ Graygram/PostCardCell.swift
@@ -10,11 +10,16 @@ import UIKit
 
 final class PostCardCell: UICollectionViewCell {
 
+  fileprivate let photoView = UIImageView()
   fileprivate let messageLabel = UILabel()
 
   override init(frame: CGRect) {
     super.init(frame: frame)
+
+    self.photoView.backgroundColor = .gray
     self.messageLabel.numberOfLines = 3
+
+    self.contentView.addSubview(self.photoView)
     self.contentView.addSubview(self.messageLabel)
   }
   
@@ -24,12 +29,17 @@ final class PostCardCell: UICollectionViewCell {
 
   func configure(post: Post) {
     self.backgroundColor = .lightGray
+    // TODO: photoView 이미지 설정
     self.messageLabel.text = post.message
     self.setNeedsLayout()
   }
 
   override func layoutSubviews() {
     super.layoutSubviews()
+
+    self.photoView.frame.size.width = self.contentView.frame.width
+    self.photoView.frame.size.height = self.photoView.frame.size.width
+
     self.messageLabel.frame.size.width = self.contentView.frame.width
     self.messageLabel.sizeToFit()
   }
