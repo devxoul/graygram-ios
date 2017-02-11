dd1c86a83376bd4f63bdb613bf82139ef247c571
Suyeol Jeon <devxoul@gmail.com>
2017-02-12 02:05:38 +0900

    UI 상수들을 Metric 구조체로 정의

--- Graygram/PostCardCell.swift
+++ Graygram/PostCardCell.swift
@@ -7,10 +7,18 @@
 //
 
 import UIKit
+import CGFloatLiteral
 import Kingfisher
 import ManualLayout
 
 final class PostCardCell: UICollectionViewCell {
+
+  fileprivate struct Metric {
+    static let messageLabelTop = 10.f
+    static let messageLabelLeft = 10.f
+    static let messageLabelRight = 10.f
+    static let messageLabelBottom = 10.f
+  }
 
   fileprivate let photoView = UIImageView()
   fileprivate let messageLabel = UILabel()
@@ -47,10 +55,11 @@ final class PostCardCell: UICollectionViewCell {
 
     if let message = post.message, !message.isEmpty {
       let font = UIFont.systemFont(ofSize: 14)
-      let messageLabelSize = message.size(width: width - 20, font: font, numberOfLines: 3)
-      height += 10 // messageLabel top
+      let messageLabelWidth = width - Metric.messageLabelLeft - Metric.messageLabelRight
+      let messageLabelSize = message.size(width: messageLabelWidth, font: font, numberOfLines: 3)
+      height += Metric.messageLabelTop
       height += messageLabelSize.height // messageLabel height
-      height += 10 // messageLabel bottom
+      height += Metric.messageLabelBottom
     }
     return CGSize(width: width, height: height)
   }
@@ -61,9 +70,9 @@ final class PostCardCell: UICollectionViewCell {
     self.photoView.width = self.contentView.width
     self.photoView.height = self.photoView.width
 
-    self.messageLabel.top = self.photoView.bottom + 10
-    self.messageLabel.left = 10
-    self.messageLabel.width = self.contentView.width - 20
+    self.messageLabel.top = self.photoView.bottom + Metric.messageLabelTop
+    self.messageLabel.left = Metric.messageLabelLeft
+    self.messageLabel.width = self.contentView.width - Metric.messageLabelLeft - Metric.messageLabelRight
     self.messageLabel.sizeToFit()
   }
 
