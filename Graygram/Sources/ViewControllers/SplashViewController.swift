d7ccb29b7b2c4dce64275dae6e3a1a3ff0167845
Suyeol Jeon <devxoul@gmail.com>
2017-02-21 04:30:02 +0900

    빈 SplashViewController 작성

---
+++ Graygram/Sources/ViewControllers/SplashViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SplashViewController.swift
+//  Graygram
+//
+//  Created by Suyeol Jeon on 21/02/2017.
+//  Copyright © 2017 Suyeol Jeon. All rights reserved.
+//
+
+import UIKit
+
+final class SplashViewController: UIViewController {
+
+}
