20ec1b085e244a7e7356a42a11e79ca70762d7eb
Suyeol Jeon <devxoul@gmail.com>
2017-03-05 03:37:43 +0900

    업로드 버튼 역할을 할 가짜 뷰 컨트롤러를 탭바에 추가

--- Graygram/Sources/ViewControllers/MainTabBarController.swift
+++ Graygram/Sources/ViewControllers/MainTabBarController.swift
@@ -12,10 +12,18 @@ final class MainTabBarController: UITabBarController {
 
   let feedViewController = FeedViewController()
 
+  /// 업로드 버튼을 할 가짜 뷰 컨트롤러. 실제로 선택되지는 않습니다.
+  let fakeUploadViewController = UIViewController().then {
+    $0.tabBarItem.image = UIImage(named: "tab-upload")
+    $0.tabBarItem.imageInsets.top = 5
+    $0.tabBarItem.imageInsets.bottom = -5
+  }
+
   init() {
     super.init(nibName: nil, bundle: nil)
     self.viewControllers = [
       UINavigationController(rootViewController: self.feedViewController),
+      self.fakeUploadViewController,
     ]
   }
   
