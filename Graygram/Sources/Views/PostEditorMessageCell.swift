235e7fa87b4f49a8a688752393c0344cb2ecaade
Suyeol Jeon <devxoul@gmail.com>
2017-03-08 02:28:36 +0900

    PostEditorMessageCell 최소 높이에 텍스트 뷰 inset을 더한 값을 사용

--- Graygram/Sources/Views/PostEditorMessageCell.swift
+++ Graygram/Sources/Views/PostEditorMessageCell.swift
@@ -48,7 +48,7 @@ final class PostEditorMessageCell: UITableViewCell {
 
   class func height(width: CGFloat, message: String?) -> CGFloat {
     let messageHeight = message?.size(width: width, font: Font.textView).height ?? 0
-    let minimumHeight = ceil(Font.textView.lineHeight * 3)
+    let minimumHeight = ceil(Font.textView.lineHeight * 3) + 10 + 10 // UITextView의 기본 inset
     return max(messageHeight, minimumHeight)
   }
 
