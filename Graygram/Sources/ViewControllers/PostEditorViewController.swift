fe0affb6d2e3a1b48b7aaca0161efd8f34fe1780
Suyeol Jeon <devxoul@gmail.com>
2017-03-08 02:17:49 +0900

    indexPath.row를 분기처리하여 이미지 셀과 메시지 셀 구분

--- Graygram/Sources/ViewControllers/PostEditorViewController.swift
+++ Graygram/Sources/ViewControllers/PostEditorViewController.swift
@@ -53,13 +53,24 @@ final class PostEditorViewController: UIViewController {
 extension PostEditorViewController: UITableViewDataSource {
 
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 1
+    return 2
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "imageCell", for: indexPath) as! PostEditorImageCell
-    cell.configure(image: self.image)
-    return cell
+    switch indexPath.row {
+    case 0:
+      let cell = tableView.dequeueReusableCell(withIdentifier: "imageCell", for: indexPath) as! PostEditorImageCell
+      cell.configure(image: self.image)
+      return cell
+
+    case 1:
+      let cell = tableView.dequeueReusableCell(withIdentifier: "messageCell", for: indexPath) as! PostEditorMessageCell
+      cell.configure(message: self.message)
+      return cell
+
+    default:
+      return UITableViewCell()
+    }
   }
 
 }
@@ -67,7 +78,16 @@ extension PostEditorViewController: UITableViewDataSource {
 extension PostEditorViewController: UITableViewDelegate {
 
   func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    return PostEditorImageCell.height(width: tableView.width)
+    switch indexPath.row {
+    case 0:
+      return PostEditorImageCell.height(width: tableView.width)
+
+    case 1:
+      return PostEditorMessageCell.height(width: tableView.width, message: self.message)
+
+    default:
+      return 0
+    }
   }
 
 }
