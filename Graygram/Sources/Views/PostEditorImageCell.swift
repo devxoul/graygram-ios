55ffe342bb7565e072f2f6786bbe6bd7cbc8f485
Suyeol Jeon <devxoul@gmail.com>
2017-03-08 01:59:34 +0900

    PostEditorImageCell에 photoView 추가

--- Graygram/Sources/Views/PostEditorImageCell.swift
+++ Graygram/Sources/Views/PostEditorImageCell.swift
@@ -10,14 +10,27 @@ import UIKit
 
 final class PostEditorImageCell: UITableViewCell {
 
+  // MARK: UI
+
+  fileprivate let photoView = UIImageView()
+
+
   // MARK: Initializing
 
   override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
+    self.contentView.addSubview(self.photoView)
   }
   
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+
+
+  // MARK: Configuring
+
+  func configure(image: UIImage) {
+    self.photoView.image = image
   }
 
 }
