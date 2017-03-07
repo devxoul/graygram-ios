7dfc54533750935f5f7034d17af58964dc9e8b05
Suyeol Jeon <devxoul@gmail.com>
2017-03-08 02:12:58 +0900

    PostEditorMessageCell에서 message를 옵셔널로 처리할 수 있도록 함

--- Graygram/Sources/Views/PostEditorMessageCell.swift
+++ Graygram/Sources/Views/PostEditorMessageCell.swift
@@ -38,7 +38,7 @@ final class PostEditorMessageCell: UITableViewCell {
 
   // MARK: Configuring
 
-  func configure(message: String) {
+  func configure(message: String?) {
     self.textView.text = message
     self.setNeedsLayout()
   }
@@ -46,8 +46,8 @@ final class PostEditorMessageCell: UITableViewCell {
 
   // MARK: Size
 
-  class func height(width: CGFloat, message: String) -> CGFloat {
-    let messageHeight = message.size(width: width, font: Font.textView).height
+  class func height(width: CGFloat, message: String?) -> CGFloat {
+    let messageHeight = message?.size(width: width, font: Font.textView).height ?? 0
     let minimumHeight = ceil(Font.textView.lineHeight * 3)
     return max(messageHeight, minimumHeight)
   }
