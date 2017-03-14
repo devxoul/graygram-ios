11adc87da8992ac66c5d79bb63b8a845053705e8
Suyeol Jeon <devxoul@gmail.com>
2017-03-15 03:49:01 +0900

    FeedViewMode 타입 추가

---
+++ Graygram/Sources/Types/FeedViewMode.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewMode.swift
+//  Graygram
+//
+//  Created by Suyeol Jeon on 15/03/2017.
+//  Copyright © 2017 Suyeol Jeon. All rights reserved.
+//
+
+enum FeedViewMode {
+  case card
+  case tile
+}
