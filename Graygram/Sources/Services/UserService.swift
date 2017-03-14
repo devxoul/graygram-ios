7e5607faecfe38a29393f183317b3bc14a2b8013
Suyeol Jeon <devxoul@gmail.com>
2017-03-15 02:07:40 +0900

    UserService.me()에서 User 인스턴스를 생성해봄

--- Graygram/Sources/Services/UserService.swift
+++ Graygram/Sources/Services/UserService.swift
@@ -7,6 +7,28 @@
 //
 
 import Alamofire
+import ObjectMapper
 
 struct UserService {
+
+  static func me(_ completion: @escaping (DataResponse<User>) -> Void) {
+    let urlString = "https://api.graygram.com/me"
+    Alamofire.request(urlString)
+      .validate(statusCode: 200..<400)
+      .responseJSON { response in
+        switch response.result {
+        case .success(let json):
+          if let user = Mapper<User>().map(JSONObject: json) {
+            // 성공
+          } else {
+            // 실패
+          }
+
+        case .failure(let error):
+          // 실패
+          break
+        }
+      }
+  }
+
 }
