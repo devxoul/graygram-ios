40a6983286bf49c4fbea5e54d6f12d3855636088
Suyeol Jeon <devxoul@gmail.com>
2017-03-15 02:12:04 +0900

    MappingError 추가

---
+++ Graygram/Sources/Networking/MappingError.swift
@@ -0,0 +1,10 @@
+//
+//  MappingError.swift
+//  Graygram
+//
+//  Created by Suyeol Jeon on 15/03/2017.
+//  Copyright © 2017 Suyeol Jeon. All rights reserved.
+//
+
+struct MappingError: Error {
+}
