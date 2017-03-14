71332cf27ea0213e5bdb674e2d7b802095a40079
Suyeol Jeon <devxoul@gmail.com>
2017-03-15 03:47:19 +0900

    PostTileCell 작성

---
+++ Graygram/Sources/Views/PostTileCell.swift
@@ -0,0 +1,48 @@
+//
+//  PostTileCell.swift
+//  Graygram
+//
+//  Created by Suyeol Jeon on 15/03/2017.
+//  Copyright © 2017 Suyeol Jeon. All rights reserved.
+//
+
+import UIKit
+
+final class PostTileCell: UICollectionViewCell {
+
+  fileprivate let photoView = UIImageView().then {
+    $0.backgroundColor = .lightGray
+  }
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    self.contentView.addSubview(self.photoView)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+
+  // MARK: Configuring
+
+  func configure(post: Post) {
+    self.photoView.setImage(with: post.photoID, size: .thumbnail)
+  }
+
+
+  // MARK: Size
+
+  class func size(width: CGFloat, post: Post) -> CGSize {
+    return CGSize(width: width, height: width) // 정사각형
+  }
+
+
+  // MARK: Layout
+
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    self.photoView.frame = self.contentView.bounds
+  }
+
+}
