67f91133d124a97b7eddc28a9f644ef3f8de8d65
Suyeol Jeon <devxoul@gmail.com>
2017-03-15 01:26:54 +0900

    빈 UserService 작성

---
+++ Graygram/Sources/Services/UserService.swift
@@ -0,0 +1,12 @@
+//
+//  UserService.swift
+//  Graygram
+//
+//  Created by Suyeol Jeon on 14/03/2017.
+//  Copyright © 2017 Suyeol Jeon. All rights reserved.
+//
+
+import Alamofire
+
+struct UserService {
+}
