78ca61fc90a4529071c0b7b8252a232a53ca8443
Suyeol Jeon <devxoul@gmail.com>
2017-03-15 02:59:59 +0900

    DataResponse.map()을 제거하고 flatMap() 작성

--- Graygram/Sources/Networking/DataResponse+MapResult.swift
+++ Graygram/Sources/Networking/DataResponse+MapResult.swift
@@ -10,15 +10,23 @@ import Alamofire
 
 extension DataResponse {
 
-  /// `DataResponse`에 새로운 `Result`를 맵핑하여 반환합니다.
-  func map<T>(_ transform: (Result<Value>) -> Result<T>) -> DataResponse<T> {
+  /// `Result`의 값을 가지고 만든 새로운 `Result`를 사용하는 `DataResponse`를 반환합니다.
+  func flatMap<T>(_ transform: (Value) -> Result<T>) -> DataResponse<T> {
+    let result: Result<T>
+    switch self.result {
+    case .success(let value):
+      result = transform(value)
+    case .failure(let error):
+      result = .failure(error)
+    }
     return DataResponse<T>(
       request: self.request,
       response: self.response,
       data: self.data,
-      result: transform(self.result),
+      result: result,
       timeline: self.timeline
     )
+
   }
 
 }
