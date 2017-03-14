bd4c5ba5c79c3aea4adedd2787db88948a1d2f11
Suyeol Jeon <devxoul@gmail.com>
2017-03-15 02:28:50 +0900

    SplashViewController에서 UserService.me()를 사용하도록 함

--- Graygram/Sources/ViewControllers/SplashViewController.swift
+++ Graygram/Sources/ViewControllers/SplashViewController.swift
@@ -15,20 +15,17 @@ final class SplashViewController: UIViewController {
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
 
-    let urlString = "https://api.graygram.com/me"
-    Alamofire.request(urlString)
-      .validate(statusCode: 200..<400)
-      .responseJSON { response in
-        switch response.result {
-        case .success(let value):
-          print("내 프로필 정보 받아오기 성공!", value)
-          AppDelegate.instance?.presentMainSreen()
+    UserService.me() { response in
+      switch response.result {
+      case .success(let value):
+        print("내 프로필 정보 받아오기 성공!", value)
+        AppDelegate.instance?.presentMainSreen()
 
-        case .failure(let error):
-          print("내 프로필 정보 받아오기 실패 ㅠㅠ", error)
-          AppDelegate.instance?.presentLoginSreen()
-        }
+      case .failure(let error):
+        print("내 프로필 정보 받아오기 실패 ㅠㅠ", error)
+        AppDelegate.instance?.presentLoginSreen()
       }
+    }
   }
 
 }
