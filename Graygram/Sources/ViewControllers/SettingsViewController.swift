3c3ab20f840c484b1647b5656ef2a6377963937d
Suyeol Jeon <devxoul@gmail.com>
2017-03-20 03:47:49 +0900

    SettingsViewController 탭바 이미지와 제목 설정

--- Graygram/Sources/ViewControllers/SettingsViewController.swift
+++ Graygram/Sources/ViewControllers/SettingsViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 final class SettingsViewController: UIViewController {
 
+  init() {
+    super.init(nibName: nil, bundle: nil)
+    self.tabBarItem.title = "Settings"
+    self.tabBarItem.image = UIImage(named: "tab-settings")
+    self.tabBarItem.selectedImage = UIImage(named: "tab-settings-selected")
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
   override func viewDidLoad() {
     super.viewDidLoad()
   }
