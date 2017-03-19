d85a738765fd6452d935e3180bba7ff453fccac5
Suyeol Jeon <devxoul@gmail.com>
2017-03-20 04:01:16 +0900

    SettingsCell 추가

---
+++ Graygram/Sources/Views/SettingsCell.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsCell.swift
+//  Graygram
+//
+//  Created by Suyeol Jeon on 20/03/2017.
+//  Copyright © 2017 Suyeol Jeon. All rights reserved.
+//
+
+import UIKit
+
+final class SettingsCell: UITableViewCell {
+
+  override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+    super.init(style: .value1, reuseIdentifier: reuseIdentifier)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+}
