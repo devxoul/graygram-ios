2334f25619809e20d8bdec43b1c5a7f62c58d093
Suyeol Jeon <devxoul@gmail.com>
2017-03-20 03:45:58 +0900

    SettingsViewController 추가

---
+++ Graygram/Sources/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  Graygram
+//
+//  Created by Suyeol Jeon on 20/03/2017.
+//  Copyright © 2017 Suyeol Jeon. All rights reserved.
+//
+
+import UIKit
+
+final class SettingsViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
