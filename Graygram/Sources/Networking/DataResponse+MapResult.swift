a2c99779da32d06ccecb2d68556746878ce777b8
Suyeol Jeon <devxoul@gmail.com>
2017-06-29 00:07:04 +0900

    flatMap을 flatMapResult로 리네임

--- Graygram/Sources/Networking/DataResponse+MapResult.swift
+++ Graygram/Sources/Networking/DataResponse+MapResult.swift
@@ -11,7 +11,7 @@ import Alamofire
 extension DataResponse {
 
   /// `Result`의 값을 가지고 만든 새로운 `Result`를 사용하는 `DataResponse`를 반환합니다.
-  func flatMap<T>(_ transform: (Value) -> Result<T>) -> DataResponse<T> {
+  func flatMapResult<T>(_ transform: (Value) -> Result<T>) -> DataResponse<T> {
     let result: Result<T>
     switch self.result {
     case .success(let value):
